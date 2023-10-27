531c1639052cdfc8cc71eab601c1d39c190fb642
wesleye2 <wesleye2@illinois.edu>
2023-10-26 22:08:19 -0700

    Updated with new error message when email is not in correct format when logging in

--- app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
@@ -125,8 +125,13 @@ public class LoginActivity extends AppCompatActivity {
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LoginActivity.this, "Authentication failed. Try again.",
-                                            Toast.LENGTH_SHORT).show();
+                                    if (task.getException().getMessage() == "The email address is badly formatted.") {
+                                        Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Toast.makeText(LoginActivity.this, "Authentication failed. Try again.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
                                     passwordInput.getText().clear();
                                     //updateUI(null);
                                 }
